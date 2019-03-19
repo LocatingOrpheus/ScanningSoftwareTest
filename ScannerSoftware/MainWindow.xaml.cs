f5a0f8cc3efc23ee0bc312ea2b7b6985e4f82639
Tony Bearden <beardent@my.lanecc.edu>
2019-03-19 13:40:25 -0700

    InitialCommit

---
+++ ScannerSoftware/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WIA;
+
+namespace ScannerSoftware
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DeviceManager device;
+        public MainWindow()
+        {
+            InitializeComponent();
+            device = new DeviceManager();
+        }
+
+        private void GetScannerBtnClick(object sender, RoutedEventArgs e)
+        {
+            for (int i = 1; i <= device.DeviceInfos.Count; i++)
+            {
+                if (device.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    continue;
+                ScannerNameLbl.Content = device.DeviceInfos[i].Properties["Name"].get_Value();
+            }
+        }
+
+        private void ScanBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ScannerSoftware/ScannerCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WIA;
+
+namespace ScannerSoftware
+{
+    public class ScannerCtrl
+    {
+        public DeviceManager device;
+        public DeviceInfo FirstScanner = null;
+
+
+        public ScannerCtrl()
+        {
+            device = new DeviceManager();
+        }
+        /*public List<DeviceManager> ListDevices()
+        {
+            List<DeviceInfos> lst = device.DeviceInfos;
+        }*/
+    }
+}
