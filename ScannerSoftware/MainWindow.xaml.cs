7a1b863399fbb2290eae127157954588ec63b492
Tony Bearden <beardent@my.lanecc.edu>
2019-03-22 05:53:21 -0700

    Added local changes

--- ScannerSoftware/MainWindow.xaml.cs
+++ ScannerSoftware/MainWindow.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
+﻿using System.Windows;
 using WIA;
 
 namespace ScannerSoftware
@@ -23,7 +9,6 @@ namespace ScannerSoftware
     public partial class MainWindow : Window
     {
         DeviceManager deviceManager;
-        DeviceInfo deviceInfo = null;
         public MainWindow()
         {
             InitializeComponent();
@@ -44,20 +29,8 @@ namespace ScannerSoftware
 
         private void ScanBtn_Click(object sender, RoutedEventArgs e)
         {
-            for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
-            {
-                if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
-                    continue;
-                deviceInfo = deviceManager.DeviceInfos[i];
-                break;
-            }
-            var device = deviceInfo.Connect();
-            var scannerItem = device.Items[1];
-            var imageFile = (ImageFile)scannerItem.Transfer(FormatID.wiaFormatJPEG);
-            var path = @"I:\scan.jpg";
-            if (File.Exists(path))
-                File.Delete(path);
-            imageFile.SaveFile(path);
+            ScannerCtrl scannerCtrl = new ScannerCtrl();
+            scannerCtrl.ScanDoc();
         }
     }
 }
--- ScannerSoftware/ScannerCtrl.cs
+++ ScannerSoftware/ScannerCtrl.cs
@@ -3,23 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using WIA;
 
 namespace ScannerSoftware
 {
     public class ScannerCtrl
     {
-        public DeviceManager device;
-        public DeviceInfo FirstScanner = null;
+        DeviceManager deviceManager;
+        DeviceInfo deviceInfo = null;
 
 
         public ScannerCtrl()
         {
-            device = new DeviceManager();
+            deviceManager = new DeviceManager();
         }
         /*public List<DeviceManager> ListDevices()
         {
             List<DeviceInfos> lst = device.DeviceInfos;
         }*/
+        public void ScanDoc()
+        {
+            for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+            {
+                if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    continue;
+                deviceInfo = deviceManager.DeviceInfos[i];
+                break;
+            }
+            var device = deviceInfo.Connect();
+            var scannerItem = device.Items[1];
+            var imageFile = (ImageFile)scannerItem.Transfer(FormatID.wiaFormatJPEG);
+            var path = @"I:\scan.jpg";
+            if (File.Exists(path))
+                File.Delete(path);
+            imageFile.SaveFile(path);
+        }
     }
 }
